c0a1abb94a833839a805efebc936182cf561c9ba
vrlevcphilips <viktor.levchenko@philips.com>
2025-12-23 23:33:03 +0200

    Lobby - done.

--- Lobby/Lobby.cpp
+++ Lobby/Lobby.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <assert.h>
+
 using namespace std;
 
 class ISolution
@@ -13,23 +15,65 @@ public:
 
 static void ReadPazzleInput(const string& filepath, ISolution* solution);
 
+string SUM(const string& A, const string& B)
+{
+	int a = 0, b = 0, d = 0;
+	string sum;
+	auto i = A.rbegin(); auto j = B.rbegin();
+	for (; i != A.rend() || j != B.rend();)
+	{
+		a = i != A.rend() ? *i - '0' : 0;
+		b = j != B.rend() ? *j - '0' : 0;
+		int s = a + b + d; d = s / 10;
+		sum.push_back(char((int)'0' + (s % 10)));
+		if (i != A.rend()) ++i;
+		if (j != B.rend()) ++j;
+	}
+	if (d > 0) sum.push_back(char((int)'0' + d));
+	for (int i = 0, j = (int)sum.length() - 1; i < j; ++i, --j) swap(sum[i], sum[j]);
+	return sum;
+}
+
 class  Solution : public ISolution
 {
 	int res = 0;
-	int joltage = 0;
+	string sum;
 public:
+	Solution() : sum("0") {}
 	void SetFail() override { res = -1; }
 	void Process(const string& line) override
 	{
-		joltage += MaxJoltage(line);
+		string jolgate = MaxJoltage(line);
+		sum = SUM(sum, jolgate);
 	}
 	int Result() const override { return res; }
-	void PrintResult() { cout << endl << "Maximum joltage: " << joltage << endl; }
+	void PrintResult() { cout << endl << "Maximum joltage: " << sum << endl; }
 private:
-	int MaxJoltage(const string& line)
+	string MaxJoltage(const string& line)
 	{
-		cout << "    Max joltage: " << 0 << endl;
-		return 0;
+		string joltage_max = line.substr(line.length() - 12);
+		assert(joltage_max.length() == 12);
+
+		for (int i = line.length() - 12 - 1; 0 <= i; --i)
+		{
+			int new_dig = static_cast<int>(line[i] - '0');
+			int cur_dig = static_cast<int>(joltage_max[0] - '0');
+			if (new_dig >= cur_dig)
+			{
+				string joltage_cur;
+				for (int j = 0; j < 12; ++j)
+				{
+					string cur = joltage_max;
+					cur.erase(j, 1);
+					cur = to_string(new_dig) + cur;
+					if (joltage_cur.empty() || joltage_cur < cur) joltage_cur = cur;
+				}
+				if (joltage_max < joltage_cur) joltage_max = joltage_cur;
+			}
+		}
+
+		cout << "    Max joltage: " << joltage_max << endl;
+		return joltage_max;
 	}
 };
 
