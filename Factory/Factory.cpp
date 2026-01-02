03c1f569d4b5aedecf5c78e43b06133b9aed2478
vrlevc <levchenko.victor@gmail.com>
2026-01-02 10:23:19 +0200

    Factory - In progress....

--- Factory/Factory.cpp
+++ Factory/Factory.cpp
@@ -8,18 +8,100 @@
 using namespace std;
 
 using INT = unsigned short;
+INT INT_INF = UINT16_MAX;
 
 class Machine
 {
 	INT L = 0;		// Indicator
 	vector<INT> B;  // Buttons
+	vector<INT> J;  // Jvoltage
 public:
 	static Machine ReadMachine(const string& line); // [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
 	static INT ReadLights(const string& line); // .##.
 	static INT ReadButton(const string& line); // 1,3
 	INT MinCombination();
 	bool IsPossible(INT N, INT CURR, INT ID);
-	int ButtonsNum() const { return B.size(); }
+	int ButtonsNum() const { return (int)B.size(); }
+};
+
+struct Range
+{
+	INT a, b;
+	static Range Cross(const Range& a, const Range& b)
+	{
+		assert(a.a <= a.b); assert(b.a <= b.b);
+		Range cross = { max(a.a, b.a), min(a.b, b.b) };
+		assert(cross.a <= cross.b);
+		return cross;
+	}
+};
+
+class Junit
+{
+	INT S; // sum of 0*x0 + 1*x1 + ...
+	vector<INT> J; // 0 1 1 0
+public:
+	Junit(INT S, const vector<INT> J) : S(S), J(J) {}
+	bool operator==(const Junit& JU) const 
+	{
+		if (S != JU.S) return false;
+		for (int i = 0; i < J.size(); ++i) if (J[i] != JU.J[i]) return false;
+		return true; 
+	}
+	vector<Range> GetRanges() const
+	{
+		int rang = 0; for (const auto& r : J) if (r == 1) ++rang;
+		vector<Range> ranges(J.size(), Range{0, INT_INF});
+		for (int i = 0; i < J.size(); ++i) 
+			if (J[i] == 1) 
+				ranges[i] = ( rang == 1 ? Range{ S, S} : Range{ 0, S } );
+		return ranges;
+	}
+	int VariablesCount() const { return (int)J.size(); }
+};
+
+class Jvoltage
+{
+	vector<Junit> R;
+public:
+	// [B0(Jn-1, ..., J0),..., Bn-1(Jn-1,...,J0)] [S0,...,Sn-1]
+	Jvoltage(const vector<INT>& B, const vector<INT>& S)
+	{
+		for (int j = (int)S.size() - 1; 0 <= j; --j)
+		{
+			INT s = S[j]; // jvoltage for j indes start from 0
+			INT i = 1 << j;
+			vector<INT> r(B.size(), 0);
+			for (int b = 0; b < B.size(); ++b)
+			{
+				if (B[b] & i) r[b] = 1;
+			}
+			Junit junit(s, r);
+			bool add = true;
+			for (const Junit& ju : R) if (ju == junit) { add = false; break; }
+			if (add) R.push_back(junit);
+		}
+	}
+
+	void Solve()
+	{
+		vector<Range> BOUNDS = R[0].GetRanges();
+		for (const Junit& ju : R)
+		{
+			vector<Range> bounds = ju.GetRanges();
+			for (int i = 0; i < BOUNDS.size(); ++i) BOUNDS[i] = Range::Cross(BOUNDS[i], bounds[i]);
+		}
+
+		long long int N = 1;
+		for (const Range& r : BOUNDS)
+		{
+			assert(r.b != INT_INF);
+			N *= r.b - r.a + 1;
+		}
+		string solution = (R.size() >= R[0].VariablesCount() ? "SOLVE  " : "ITERATE");
+		cout << solution << " - Irterations : " << N;
+		cout << "  EQUATIONS: " << R.size() << "  VARIABLES: " << R[0].VariablesCount() << endl;
+	}
 };
 
 class Factory
@@ -49,7 +131,7 @@ void PrintID(INT id, size_t LEN)
 		ids.push_back( id & k ? '1' : '0');
 		k <<= 1;
 	}
-	for (int i = max(ids.length(), LEN) - 1; 0 <= i; --i)
+	for (int i = (int)max(ids.length(), LEN) - 1; 0 <= i; --i)
 	{
 		if (i < ids.length()) cout << ids[i];
 		else cout << '0';
@@ -116,19 +198,35 @@ Machine Machine::ReadMachine(const string& line)
 	// Read lights:
 	assert(line[0] == '[');
 	int l = 1;
-	int r = l+1;
+	int r = l + 1;
 	for (; r < line.length(); ++r) if (line[r] == ']') break;
 	m.L = ReadLights(line.substr(l, (r++) - l)); l = r;
 	while (line[r] != '{')
 	{
-		for (; r < line.length(); ++r) { if (line[r] == '(') break; } 
+		for (; r < line.length(); ++r) { if (line[r] == '(') break; }
 		l = ++r;
-		for (; r < line.length(); ++r) { if (line[r] == ')') break; } 
+		for (; r < line.length(); ++r) { if (line[r] == ')') break; }
 		m.B.push_back(ReadButton(line.substr(l, r - l)));
 		++r;
 		while (line[r] == ' ') ++r;
 	}
 	for (int i = 0; i < m.B.size() / 2; ++i) swap(m.B[i], m.B[m.B.size() - 1 - i]);
+	l = ++r;
+	while (line[r] != '}')
+	{
+		if (line[r] == ',')
+		{
+			m.J.push_back((INT)stoi(line.substr(l, r - l)));
+			l = r + 1;
+		}
+		++r;
+	}
+	m.J.push_back((INT)stoi(line.substr(l, r - l)));
+
+	// [B0(Jn-1, ..., J0),..., Bn-1(Jn-1,...,J0)] [S0,...,Sn-1]
+	Jvoltage jv(m.B, m.J); 
+	jv.Solve();
+
 	return m;
 }
 
